2d85491d7bea27622b7852de7ff3415a4744c057
brunoc-cardoso <brunocpessoal@gmail.com>
2022-11-06 14:51:29 -0300

    add style on signIn page

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,26 @@
-import { Center, Text } from 'native-base';
+/* eslint-disable react/jsx-one-expression-per-line */
+import Logo from '@/assets/logo.svg';
+import { Fontisto } from '@expo/vector-icons';
+import { Center, Icon, Text } from 'native-base';
+
+import { Button } from '@/components/Button';
 
 type SignInProps = {};
 
 export function SignIn({}: SignInProps) {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>
-        SignIn Screen
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM O GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        marginTop={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   );
